66b42fe9f6219c75b60d3024265ac2fdcacac04a
masoud-nasiri <35027884+mass-nasiri@users.noreply.github.com>
2026-05-26 10:43:52 +0330

    Create App.xaml.cs

---
+++ App.xaml.cs
@@ -0,0 +1,10 @@
+// App.xaml.cs
+using System.Windows;
+
+namespace MinimalMusicPlayer
+{
+    public partial class App : Application
+    {
+        // نقطه ورود به صورت خودکار توسط بیلد سیستم WPF تولید و مدیریت می‌شود
+    }
+}
